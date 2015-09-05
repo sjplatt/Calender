2ee50235d93c4aef9ab3cb9588ec630b64b00260
Matthew Zhu <matt.z496@gmail.com>
2015-09-05 02:31:37 -0400

    Prepare for merge

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,31 +2,30 @@ class WelcomeController < ApplicationController
   def index
   end
 
-
-  def process_user_info()
-
+  # GET '/process_user_info'
+  def process_user_info
+    
   end
 
   # POST 'ajax/get_fb_data'
   def first_page
     self_data = params[:self]
-    friends_data = params[:friends]
+    #friends_data = params[:friends]
 
-    friends_ids = []
-    friends_names = []
-    friends_data.each do |key,friend|
-      friends_ids.push(friend["id"])
-      friends_names.push(friend["name"])
-    end
+    # friends_ids = []
+    # friends_names = []
+    # friends_data.each do |key,friend|
+    #   friends_ids.push(friend["id"])
+    #   friends_names.push(friend["name"])
+    # end
 
-    output = {:self => self_data, :friends => friends_ids}
+    #output = {:self => self_data, :friends => friends_ids}
 
     if !User.find_by(facebookid:self_data["id"])
       render :js => "window.location = '/process_user_info'"
-      return
+    else
+      main_page(self_data["id"],self_data["id"])
     end
-
-    main_page(self_data["id"],self_data["id"])
   end
 
   def main_page(login_id,view_id)
