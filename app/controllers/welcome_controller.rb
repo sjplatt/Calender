c115816ef7a0f795eb26f27802f59cc9c0ec3d95
Matthew Zhu <matt.z496@gmail.com>
2015-09-05 00:12:43 -0400

    Send self and friend data with ajax

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,22 @@ class WelcomeController < ApplicationController
   def index
   end
 
+  # POST 'ajax/get_fb_data'
   def first_page
-    
+    self_data = params[:self]
+    friends_data = params[:friends]
+
+    friends_names = []
+    friends_data.each do |key,friend|
+      friends_names.push(friend["name"])
+    end
+
+    output = {:self => self_data, :friends => friends_names}
+    puts output
+
+    respond_to do |format|
+      format.json {render :json => output}
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  post '/ajax/get_fb_data' => 'welcome#first_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
